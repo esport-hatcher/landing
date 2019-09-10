29936d41c04091c4b14576a361a8882ad0e769f1
Melliti <r.melliti@protonmail.com>
2019-09-10 14:29:59 +0200

    base of the project. [add] menu component start

---
+++ src/components/menu/Menu.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class Menu extends React.Component {
+    render () {
+        return (
+            <div className="menu">
+                <div className="menu-content container debug">
+                    
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Menu;
