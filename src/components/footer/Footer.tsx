a62febe87a197f87e0e73d217ea974a9e0f07e17
Melliti <r.melliti@protonmail.com>
2019-10-23 12:33:14 -0400

    Start the footer. Grid for the footer and footer top separator

---
+++ src/components/footer/Footer.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Footer extends React.Component {
+    render () {
+        return (
+            <div className="footer content footerGrid">
+                <div className="footerSeparator"></div>
+                {/* <div className="footerGrid"></div> */}
+            </div>
+        )
+    }
+}
+
+export default Footer;
