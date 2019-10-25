5696f235b599e1bd3212de4e0cf853b343898466
Melliti <r.melliti@protonmail.com>
2019-10-25 10:01:04 -0400

    Footer finished. Add social media icons, links and copyright sentence

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,11 +1,26 @@
 import React from 'react';
+import facebook from '../../assets/facebook_icon.png';
+import youtube from '../../assets/youtube.png';
+import twitter from '../../assets/twitter.png';
 
 class Footer extends React.Component {
     render () {
         return (
             <div className="footer content footerGrid">
                 <div className="footerSeparator"></div>
-                {/* <div className="footerGrid"></div> */}
+                <div className="footerIcons">
+                    <img src={facebook} className="media_icon"/>
+                    <img src={twitter} className="media_icon"/>
+                    <img src={youtube} className="media_icon"/>
+                </div>
+                <div className="footerLink">
+                    <a href="#" className="leftLink">A propos</a>
+                    <a href="#">Nous contacter</a>
+                </div>
+                <p>Copyright © Esport Hatcher All rights Reserved</p>
+                {/* <svg>
+                    <use href={`${facebook}`}></use>
+                </svg>*/}
             </div>
         )
     }
