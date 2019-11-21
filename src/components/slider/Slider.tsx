f0c85ad9142b067c3b6bc148d290d1b960df1ea3
Melliti <r.melliti@protonmail.com>
2019-11-21 16:11:29 -0500

    Fix height responsive on cards

--- src/components/slider/Slider.tsx
+++ src/components/slider/Slider.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Slider: React.FC = () => {
     return (
         <div className="slider">
-            <div className="content">
+            <div className="content slider-text">
                 <h3>Managez votre structure esport grâce à notre plateforme <br/> de gestion complète</h3>
                 <div className="sliderLinks">
                 <a className="subscription" href="#"><button className="buttons">S'inscrire</button></a>
