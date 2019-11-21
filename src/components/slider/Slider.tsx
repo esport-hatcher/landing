edb84967348b03721f35a8dc6611b33ef0fd7c18
Melliti <r.melliti@protonmail.com>
2019-11-21 17:30:19 -0500

    Fix responsive: slider and text inside

--- src/components/slider/Slider.tsx
+++ src/components/slider/Slider.tsx
@@ -4,7 +4,7 @@ const Slider: React.FC = () => {
     return (
         <div className="slider">
             <div className="content slider-text">
-                <h3>Managez votre structure esport grâce à notre plateforme <br/> de gestion complète</h3>
+                <h3 className="sliderTitle">Managez votre structure esport grâce à notre plateforme <br/> de gestion complète</h3>
                 <div className="sliderLinks">
                 <a className="subscription" href="#"><button className="buttons">S'inscrire</button></a>
                     <a className="information" href="#">Plus D'informations</a>
