f9b8e876657a755f9e465733abdda751002d1d39
Melliti <r.melliti@protonmail.com>
2019-12-14 10:08:51 -0500

    [fix] responsive issue with the company logo

--- src/components/menu/Categories.tsx
+++ src/components/menu/Categories.tsx
@@ -10,8 +10,36 @@ class Categories extends React.Component<{categories: string[]}> {
         return (
             <div className="categories">
                 <a href="#" className={`${this.state.toggle && "on"}`} id='toggle' onClick={this.onToggle}><span></span></a>
-
-                <ul>
+                <div className="toggle-menu">
+                    <ul>
+                        {
+                            this.props.categories.map((element, i) => {
+                                if (i === this.props.categories.length - 1) {
+                                    return (
+                                        <li key={element}>
+                                            <a href="#">
+                                                <button className="buttons menu_button">
+                                                    {element}
+                                                </button>
+                                            </a>
+                                        </li>
+                                    )
+                                }
+                                else
+                                {
+                                    return (
+                                        <li key={element}>
+                                            <a href="#">
+                                                {element}
+                                            </a>
+                                        </li>
+                                    )
+                                }
+                            })
+                        }
+                    </ul>
+                </div>
+                {/* <ul>
                     {
                         this.props.categories.map((element, i) => {
                             if (i === this.props.categories.length - 1) {
@@ -37,7 +65,7 @@ class Categories extends React.Component<{categories: string[]}> {
                             }
                         })
                     }
-                </ul>
+                </ul> */}
             </div>
         );
     }
